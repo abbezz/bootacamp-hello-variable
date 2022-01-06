b5c9637424381bfd8fd50bbcfe1b8d9a4aeffa7b
Abdollah Zamannoun <abbezz@live.com>
2022-01-06 22:50:55 +0000

    added starter code

---
+++ Unsolved/script.js
@@ -0,0 +1,25 @@
+// Write Your JavaScript Code Here
+// declare variables
+
+var personName = 'Bob';
+var pets = 2;
+var funFact = 'I am from Mars';
+
+// display messages
+
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun fact: ' + funFact + '.');
+
+// reassign variables
+
+personName = 'Alice';
+pets = 4;
+funFact = 'I am from Earth';
+
+// display messages
+
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' pet(s). ');
+console.log('Fun fact: ' + funFact + '.');
+
